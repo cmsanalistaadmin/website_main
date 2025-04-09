730429f9fe3fda0141c2d8067d4c4de30ab19e58
Patricio_Venero <jaimerodriguezechevarria@Patricio-Macbook.local>
2025-04-08 20:41:47 -0500

    v 0.3

--- proyect1/urls.py
+++ proyect1/urls.py
@@ -1,12 +1,15 @@
 # urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('supervisor/', views.supervisor_dashboard, name='supervisor_dashboard'),
     path('vendedor/', views.vendedor_dashboard, name='vendedor_dashboard'),
     path('jefe/', views.jefe_dashboard, name='jefe_dashboard'),
-    path('vendedor/formeeff/',views.formeeff, name='formeeff')
+    path('vendedor/formeeff/',views.formeeff, name='formeeff'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     # Agrega otras vistas aquí
 ]
